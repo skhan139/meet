1e94ca7613d81e163c9194fa751705a9ba7eed6b
skhan139 <146803728+skhan139@users.noreply.github.com>
2024-06-25 14:48:03 -0400

    4.6 pt 2

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -11,27 +11,34 @@ describe('show/hide an event details', () => {
         await page.waitForSelector('.event');
     });
 
-    afterAll(() => {
-        browser.close();
+    afterAll(async () => {
+        await browser.close();
     });
 
     // Scenario 1
     test('An event element is collapsed by default', async () => {
         const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeNull();
+        const isVisible = await eventDetails?.isIntersectingViewport();
+        expect(isVisible).toBe(false);
     });
 
     // Scenario 2
     test('User can expand an event to see its details', async () => {
         await page.click('.event .details-btn');
+        await page.waitForSelector('.event .details', { visible: true });
         const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeDefined();
+        const isVisible = await eventDetails.isIntersectingViewport();
+        expect(isVisible).toBe(true);
     });
 
     // Scenario 3
     test('User can collapse an event to hide details', async () => {
-        await page.click('.event .details-btn');
+        await page.click('.event .details-btn'); // Expand
+        await page.waitForSelector('.event .details', { visible: true });
+        await page.click('.event .details-btn'); // Collapse
+        await page.waitForSelector('.event .details', { hidden: true });
         const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeNull();
+        const isVisible = await eventDetails?.isIntersectingViewport();
+        expect(isVisible).toBe(false);
     });
 });
