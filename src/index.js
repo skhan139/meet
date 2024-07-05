90e3c21c94d7993a50c935c3b6f2814c08e1ff19
skhan139 <146803728+skhan139@users.noreply.github.com>
2024-07-05 00:41:50 -0400

    4.7

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('ebb178f5f2394e26beb5fc61bbcaf410').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
