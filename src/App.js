7bcc23bdae1bbe65097007e3e10152a1cdf0fc82
skhan139 <146803728+skhan139@users.noreply.github.com>
2024-07-09 20:37:26 -0400

    4.9

--- src/App.js
+++ src/App.js
@@ -1,49 +1,56 @@
 import CitySearch from './components/CitySearch';
-import EventList from './components/EventList';
-import NumberOfEvents from './components/NumberOfEvents';
-import { useEffect, useState } from 'react';
-import { extractLocations, getEvents } from './api';
-
+ import EventList from './components/EventList';
+ import NumberOfEvents from './components/NumberOfEvents';
+ import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+ import { useEffect, useState } from 'react';
+ import { extractLocations, getEvents } from './api';
  import './App.css';
-
- import { InfoAlert, ErrorAlert } from './components/Alert';
-
- const App = () => {
-   const [allLocations, setAllLocations] = useState([]);
-   const [currentNOE, setCurrentNOE] = useState(32);
-   const [events, setEvents] = useState([]);
+function App() {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
    const [currentCity, setCurrentCity] = useState("See all cities");
    const [infoAlert, setInfoAlert] = useState("");
    const [errorAlert, setErrorAlert] = useState("");
-
+   const [warningAlert, setWarningAlert] = useState("");
 
    useEffect(() => {
-    fetchData();
-  }, [currentCity, currentNOE]);
+     if (navigator.onLine) {
+       setWarningAlert("");
+     } else {
+       setWarningAlert("You are offline. The displayed list may not be up to date.");
+     }
+     fetchData();
+   }, [currentCity, currentNOE]);
+
   const fetchData = async () => {
     const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
+    let filteredEvents = currentCity === "See all cities" ?
       allEvents :
       allEvents.filter(event => event.location === currentCity)
+    // Check to see if there are events and allEvents count
+    if (currentNOE && currentNOE < filteredEvents.length) {
+      filteredEvents = filteredEvents.slice(0, currentNOE)
+    }
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
-  };
-
-   return (
-     <div className="App">
+  }
+  
+  return (
+    <div className="App">
        <div className="alerts-container">
-         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
-         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+         {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
        </div>
-       <CitySearch
+       <CitySearch 
          allLocations={allLocations}
-         setCurrentCity={setCurrentCity}
-         setInfoAlert={setInfoAlert} />
-       <NumberOfEvents
-         setCurrentNOE={setCurrentNOE}
-         setErrorAlert={setErrorAlert} />
-       <EventList events={events} />
-     </div>
-   );
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
+      <EventList events={events}/>
+    </div>
+  );
 }
 export default App;
